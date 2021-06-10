ebc205a36e9305d070e5ad3bd4a1e2b0864ca7af
Heru Handika <herubiolog@gmail.com>
2021-06-09 20:59:18 -0500

    Add working nexus code.

--- src/nexus.rs
+++ src/nexus.rs
@@ -1,104 +1,128 @@
-// use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{BufReader, Lines, Read, Result};
 
-pub fn parse_nexus_id(path: &str) {
-    let file = File::open(path).expect("CANNOT OPEN THE FILE");
-    let buff = BufReader::new(file);
+pub fn read_nexus(path: &str) {
+    let input = File::open(path).unwrap();
+    let buff = BufReader::new(input);
+    let mut nex = NexusReader::new();
+    nex.read(buff).unwrap();
 
-    let mut matrix = false;
-    let mut inblock = false;
+    let matrix = nex.parse_matrix();
 
-    // buff.read_line(buff);
-    buff.lines().filter_map(|ok| ok.ok()).for_each(|l| {
-        match l.to_lowercase() {
-            l if l.starts_with("#nexus") => (),
-            l if l.starts_with("begin") => inblock = true,
-            l if l.starts_with("matrix") => matrix = true,
-            l if l.contains(';') => matrix = false,
-            _ => (),
-        }
-
-        if matrix && inblock {
-            if !l.to_lowercase().starts_with("matrix") && l.len() > 1 {
-                let cigar: Vec<&str> = l.split_whitespace().collect();
-                println!("ID: {}", cigar[0].trim());
-                println!("{}", cigar[1].trim());
-            }
-        }
+    matrix.iter().for_each(|(id, seq)| {
+        println!(">{}", id);
+        println!("{}", seq);
     });
-
-    // let fasta = NexusReader::new(buff);
-    // fasta.into_iter().for_each(|fast| {
-    //     println!("ID: {}", fast.id);
-    //     println!("{}", fast.seq);
-    // });
 }
 
-// struct Nexus {
-//     id: String,
-//     seq: String,
-//     // ntax: usize,
-//     // nchar: usize,
-// }
+struct NexusReader {
+    matrix: String,
+}
 
-// impl Nexus {
-//     fn new() -> Self {
-//         Self {
-//             id: String::new(),
-//             seq: String::new(),
-//             // ntax: 0,
-//             // nchar: 0,
-//         }
+impl NexusReader {
+    fn new() -> Self {
+        Self {
+            matrix: String::new(),
+        }
+    }
+
+    fn read<R: Read>(&mut self, reader: R) -> Result<()> {
+        let reader = Reader::new(reader);
+        reader.into_iter().for_each(|r| {
+            if r.to_lowercase().contains("matrix") {
+                self.matrix = r.trim().to_string();
+            }
+        });
+
+        Ok(())
+    }
+
+    fn parse_matrix(&mut self) -> BTreeMap<String, String> {
+        self.matrix.pop(); // remove terminated semicolon.
+        let matrix: Vec<&str> = self.matrix.split('\n').collect();
+        let mut seqs = BTreeMap::new();
+        matrix[1..]
+            .iter()
+            .filter(|l| !l.is_empty())
+            .map(|l| l.trim())
+            .for_each(|l| {
+                let seq: Vec<&str> = l.split_whitespace().collect();
+                if seq.len() == 2 {
+                    seqs.insert(seq[0].to_string(), seq[1].to_string());
+                }
+            });
+        seqs
+    }
+}
+
+struct Reader<R> {
+    reader: Lines<BufReader<R>>,
+    buffer: String,
+    content: String,
+}
+
+impl<R: Read> Reader<R> {
+    fn new(file: R) -> Self {
+        Self {
+            reader: BufReader::new(file).lines(),
+            buffer: String::new(),
+            content: String::new(),
+        }
+    }
+}
+
+// Iterate over the file.
+// Collect each of the nexus block terminated by semi-colon.
+impl<R: Read> Iterator for Reader<R> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(Ok(line)) = self.reader.next() {
+            self.buffer.push_str(&line);
+            if !line.is_empty() {
+                self.buffer.push('\n');
+            }
+            if line.ends_with(';') {
+                self.content.push_str(&self.buffer);
+                self.buffer.clear();
+            }
+            let token = self.content.trim().to_string();
+            self.content.clear();
+            Some(token)
+        } else {
+            None
+        }
+    }
+}
+
+// #[cfg(test)]
+// mod test {
+//     use super::*;
+
+//     #[test]
+//     fn delimit_matrix_test() {
+//         let sample = "matrix\nABCDA AGTC--\n;";
+//         let (_, id) = delimit_mat(sample).unwrap();
+//         id.iter().for_each(|l| {
+//             let (res, _) = l;
+//             let s = res.to_string();
+//             assert_eq!("ABCDA", s);
+//         });
 //     }
-// }
 
-// struct NexusReader<R> {
-//     inblock: bool,
-//     matrix: bool,
-//     id: String,
-//     seq: String,
-//     reader: Lines<BufReader<R>>,
-// }
-
-// impl<R: Read> NexusReader<R> {
-//     fn new(file: R) -> Self {
-//         Self {
-//             inblock: false,
-//             matrix: false,
-//             id: String::new(),
-//             seq: String::new(),
-//             reader: BufReader::new(file).lines(),
-//         }
+//     #[test]
+//     fn regex_seq_id_test() {
+//         let text = "agga--";
+//         let re = regex_seq_id();
+//         assert_eq!(true, re.is_match(text))
 //     }
 
-//     // fn get_blocks(&self, line: &str) {}
-// }
-
-// impl<R: Read> Iterator for NexusReader<R> {
-//     type Item = Nexus;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         while let Some(Ok(line)) = self.reader.next() {
-//             match line.to_lowercase() {
-//                 l if l.starts_with("#nexus") => continue,
-//                 l if l.starts_with("begin") => self.inblock = true,
-//                 l if l.starts_with("matrix") => self.inblock = true,
-//                 l if l.starts_with("end") => self.inblock = false,
-//                 _ => (),
-//             }
-
-//             if self.inblock && self.matrix {
-//                 let mut res = Nexus::new();
-//                 let cigar: Vec<&str> = line.split('\t').collect();
-//                 res.id.push_str(&self.id);
-//                 res.seq.push_str(&self.seq);
-//                 self.seq = Some(&cigar[1]);
-//                 return Some(res);
-//             } else {
-//                  None
-//             }
-//         }
+//     #[test]
+//     fn regex_seq_end_test() {
+//         let text = "agga--\n";
+//         let re = regex_seq_id();
+//         assert_eq!(true, re.is_match(text))
 //     }
 // }
