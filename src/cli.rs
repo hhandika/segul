2ec6ad68bff7dbbdf8c4df5020ba5fd9c0b2b2e1
Heru Handika <herubiolog@gmail.com>
2021-06-16 16:18:29 -0500

    Refactor cli code. Struct is now based on the subcommands instead of filetypes.

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,14 @@
+use std::io::{self, Result, Write};
 use std::path::{Path, PathBuf};
 
 use clap::{App, AppSettings, Arg, ArgMatches};
 use glob::glob;
+use rayon::prelude::*;
 
-use crate::common::{OutputFormat, PartitionFormat};
+use crate::common::{InputFormat, OutputFormat, PartitionFormat};
 use crate::converter::Converter;
 use crate::msa;
+use crate::utils;
 
 fn get_args(version: &str) -> ArgMatches {
     App::new("segul")
@@ -240,7 +243,7 @@ fn get_args(version: &str) -> ArgMatches {
                                 .help("Sets partition format. Choice: nexus, raxml, nexsep")
                                 .takes_value(true)
                                 .required(true)
-                                .default_value("nexsep")
+                                .default_value("nexsep") 
                                 .value_name("FORMAT"),
                         )
                         .arg(
@@ -260,6 +263,8 @@ fn get_args(version: &str) -> ArgMatches {
 
 pub fn parse_cli(version: &str) {
     let args = get_args(version);
+    let text = format!("SEGUL v{}", version);
+    utils::print_divider(&text, 50);
     match args.subcommand() {
         ("convert", Some(convert_matches)) => parse_convert_subcommand(convert_matches),
         ("concat", Some(concat_matches)) => parse_concat_subcommand(concat_matches),
@@ -269,18 +274,30 @@ pub fn parse_cli(version: &str) {
 
 fn parse_convert_subcommand(args: &ArgMatches) {
     match args.subcommand() {
-        ("fasta", Some(fasta_matches)) => Fasta::new(fasta_matches).convert_fasta(),
-        ("nexus", Some(nexus_matches)) => Nexus::new(nexus_matches).convert_nexus(),
-        ("phylip", Some(phylip_matches)) => Phylip::new(phylip_matches).convert_phylip(),
+        ("fasta", Some(fasta_matches)) => {
+            ConvertParser::new(fasta_matches, InputFormat::Fasta).convert()
+        }
+        ("nexus", Some(nexus_matches)) => {
+            ConvertParser::new(nexus_matches, InputFormat::Nexus).convert()
+        }
+        ("phylip", Some(phylip_matches)) => {
+            ConvertParser::new(phylip_matches, InputFormat::Phylip).convert()
+        }
         _ => unreachable!(),
     }
 }
 
 fn parse_concat_subcommand(args: &ArgMatches) {
     match args.subcommand() {
-        ("fasta", Some(fasta_matches)) => Fasta::new(fasta_matches).concat_fasta(),
-        ("nexus", Some(nexus_matches)) => Nexus::new(nexus_matches).concat_nexus(),
-        ("phylip", Some(phylip_matches)) => Phylip::new(phylip_matches).concat_phylip(),
+        ("fasta", Some(fasta_matches)) => {
+            ConcatParser::new(fasta_matches, InputFormat::Fasta).concat()
+        }
+        ("nexus", Some(nexus_matches)) => {
+            ConcatParser::new(nexus_matches, InputFormat::Nexus).concat()
+        }
+        ("phylip", Some(phylip_matches)) => {
+            ConcatParser::new(phylip_matches, InputFormat::Phylip).concat()
+        }
         _ => unreachable!(),
     }
 }
@@ -346,51 +363,62 @@ trait Cli {
     }
 }
 
-impl Cli for Fasta<'_> {}
-impl Cli for Nexus<'_> {}
-impl Cli for Phylip<'_> {}
+impl Cli for ConvertParser<'_> {}
+impl Cli for ConcatParser<'_> {}
 
-struct Fasta<'a> {
+struct ConvertParser<'a> {
     matches: &'a ArgMatches<'a>,
+    input_format: InputFormat,
     output: PathBuf,
     is_dir: bool,
 }
 
-impl<'a> Fasta<'a> {
-    fn new(matches: &'a ArgMatches<'a>) -> Self {
+impl<'a> ConvertParser<'a> {
+    fn new(matches: &'a ArgMatches<'a>, input_format: InputFormat) -> Self {
         Self {
             matches,
+            input_format,
             output: PathBuf::new(),
             is_dir: false,
         }
     }
 
-    fn convert_fasta(&mut self) {
+    fn convert(&mut self) {
         if self.matches.is_present("input") {
-            self.convert_single_fasta();
+            self.convert_file();
         } else {
             self.convert_multiple_fasta();
         }
     }
 
-    fn convert_single_fasta(&mut self) {
+    fn convert_file(&mut self) {
         let input = Path::new(self.get_file_input(self.matches));
         self.output = self.set_output(self.matches);
-        self.convert(input);
+        self.display_input_file(input).unwrap();
+        self.convert_any(input);
     }
 
     fn convert_multiple_fasta(&mut self) {
         let dir = self.get_dir_input(self.matches);
-        let pattern = format!("{}/*.fa*", dir);
+        let pattern = self.get_pattern(dir);
         let files = self.get_files(&pattern);
+        self.output = self.set_output(&self.matches);
         self.is_dir = true;
-        files.iter().for_each(|file| {
-            self.output = self.set_output(&self.matches);
-            self.convert(file);
-        })
+        self.display_input_dir(Path::new(dir), files.len()).unwrap();
+        files.par_iter().for_each(|file| {
+            self.convert_any(file);
+        });
     }
 
-    fn convert(&self, input: &Path) {
+    fn convert_any(&self, input: &Path) {
+        match self.input_format {
+            InputFormat::Fasta => self.convert_fasta(input),
+            InputFormat::Nexus => self.convert_nexus(input),
+            InputFormat::Phylip => self.convert_phylip(input),
+        }
+    }
+
+    fn convert_fasta(&self, input: &Path) {
         if self.matches.is_present("phylip") {
             Converter::new(input, &self.output, &OutputFormat::Phylip, self.is_dir).convert_fasta();
         } else {
@@ -398,57 +426,7 @@ impl<'a> Fasta<'a> {
         }
     }
 
-    fn concat_fasta(&self) {
-        let dir = self.get_dir_input(self.matches);
-        let output = self.get_output(self.matches);
-        let output_format = self.get_output_format(self.matches);
-        let part_format = self.get_partition_format(self.matches);
-
-        msa::MSAlignment::new(dir, output, output_format, part_format).concat_fasta();
-    }
-}
-
-struct Nexus<'a> {
-    matches: &'a ArgMatches<'a>,
-    output: PathBuf,
-    is_dir: bool,
-}
-
-impl<'a> Nexus<'a> {
-    fn new(matches: &'a ArgMatches<'a>) -> Self {
-        Self {
-            matches,
-            output: PathBuf::new(),
-            is_dir: false,
-        }
-    }
-
-    fn convert_nexus(&mut self) {
-        if self.matches.is_present("input") {
-            self.convert_single_nexus();
-        } else {
-            self.convert_multiple_nexus();
-        }
-    }
-
-    fn convert_single_nexus(&mut self) {
-        let input = Path::new(self.get_file_input(self.matches));
-        self.output = self.set_output(&self.matches);
-        self.convert(input);
-    }
-
-    fn convert_multiple_nexus(&mut self) {
-        let dir = self.get_dir_input(self.matches);
-        let pattern = format!("{}/*.nex*", dir);
-        let files = self.get_files(&pattern);
-        self.is_dir = true;
-        files.iter().for_each(|file| {
-            self.output = self.set_output(&self.matches);
-            self.convert(file);
-        })
-    }
-
-    fn convert(&self, input: &Path) {
+    fn convert_nexus(&self, input: &Path) {
         if self.matches.is_present("phylip") {
             Converter::new(input, &self.output, &OutputFormat::Phylip, self.is_dir).convert_nexus();
         } else {
@@ -456,44 +434,68 @@ impl<'a> Nexus<'a> {
         }
     }
 
-    fn concat_nexus(&self) {
-        let dir = self.get_dir_input(self.matches);
-        let output = self.get_output(self.matches);
-        let output_format = self.get_output_format(self.matches);
-        let part_format = self.get_partition_format(self.matches);
+    fn convert_phylip(&self, input: &Path) {
+        if self.matches.is_present("nexus") {
+            Converter::new(input, &self.output, &OutputFormat::Nexus, self.is_dir).convert_phylip();
+        } else {
+            Converter::new(input, &self.output, &OutputFormat::Fasta, self.is_dir).convert_phylip();
+        }
+    }
 
-        msa::MSAlignment::new(dir, output, output_format, part_format).concat_nexus();
+    fn get_pattern(&self, dir: &str) -> String {
+        match self.input_format {
+            InputFormat::Fasta => format!("{}/*.fa*", dir),
+            InputFormat::Nexus => format!("{}/*.nex*", dir),
+            InputFormat::Phylip => format!("{}/*.phy*", dir),
+        }
+    }
+
+    fn display_input_file(&self, input: &Path) -> Result<()> {
+        let io = io::stdout();
+        let mut writer = io::BufWriter::new(io);
+        writeln!(writer, "Command\t: segul convert")?;
+        writeln!(writer, "Input\t: {}", &input.display())?;
+        Ok(())
+    }
+
+    fn display_input_dir(&self, input: &Path, nfile: usize) -> Result<()> {
+        let io = io::stdout();
+        let mut writer = io::BufWriter::new(io);
+        writeln!(writer, "Command\t: segul convert")?;
+        writeln!(writer, "Input dir\t: {}", &input.display())?;
+        writeln!(writer, "Total files\t: {}", nfile)?;
+        writeln!(writer, "Output dir\t: {}", self.output.display())?;
+        Ok(())
     }
 }
 
-struct Phylip<'a> {
+struct ConcatParser<'a> {
     matches: &'a ArgMatches<'a>,
-    output: PathBuf,
+    input_format: InputFormat,
 }
 
-impl<'a> Phylip<'a> {
-    fn new(matches: &'a ArgMatches<'a>) -> Self {
+impl<'a> ConcatParser<'a> {
+    fn new(matches: &'a ArgMatches<'a>, input_format: InputFormat) -> Self {
         Self {
             matches,
-            output: PathBuf::new(),
+            input_format,
         }
     }
 
-    fn convert_phylip(&self) {
-        let input = Path::new(self.get_file_input(self.matches));
-        if self.matches.is_present("nexus") {
-            Converter::new(&input, &self.output, &OutputFormat::Nexus, false).convert_phylip();
-        } else {
-            Converter::new(&input, &self.output, &OutputFormat::Fasta, false).convert_phylip();
-        }
-    }
-
-    fn concat_phylip(&self) {
+    fn concat(&self) {
         let dir = self.get_dir_input(self.matches);
         let output = self.get_output(self.matches);
         let output_format = self.get_output_format(self.matches);
         let part_format = self.get_partition_format(self.matches);
+        let mut concat = msa::MSAlignment::new(dir, output, output_format, part_format);
+        self.concat_any(&mut concat)
+    }
 
-        msa::MSAlignment::new(dir, output, output_format, part_format).concat_phylip();
+    fn concat_any(&self, concat: &mut msa::MSAlignment) {
+        match self.input_format {
+            InputFormat::Fasta => concat.concat_fasta(),
+            InputFormat::Nexus => concat.concat_nexus(),
+            InputFormat::Phylip => concat.concat_phylip(),
+        }
     }
 }
