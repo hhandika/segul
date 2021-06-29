a74ca8537ae2f7e8ee06dd49f56be04bfcab4c10
Heru Handika <herubiolog@gmail.com>
2021-06-29 18:51:34 -0500

    Add writer for dir results.

--- src/stats.rs
+++ src/stats.rs
@@ -1,5 +1,7 @@
 //! A module for sequence statistics.
 use std::collections::HashMap;
+use std::fmt::Debug;
+use std::fs::File;
 use std::io::{self, BufWriter, Result, Write};
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
@@ -8,7 +10,7 @@ use indexmap::IndexMap;
 use rayon::prelude::*;
 
 use crate::alignment::Alignment;
-use crate::common::SeqFormat;
+use crate::common::{Header, SeqFormat};
 use crate::utils;
 
 pub fn get_seq_stats(path: &Path, input_format: &SeqFormat) {
@@ -18,12 +20,12 @@ pub fn get_seq_stats(path: &Path, input_format: &SeqFormat) {
     aln.get_aln_any(path, input_format);
     spin.set_message("Counting characters...");
     let mut dna = Dna::new();
-    dna.count_chars(&aln.alignment);
+    dna.count_chars(&aln);
     spin.set_message("Getting summary stats...");
     let mut sites = Sites::new();
     sites.get_stats(&aln.alignment);
     spin.finish_with_message("DONE!\n");
-    display_stats(&sites, &dna).unwrap();
+    display_stats(&sites, &dna, &aln.header).unwrap();
 }
 
 pub fn get_stats_dir(files: &[PathBuf], input_format: &SeqFormat) {
@@ -33,37 +35,115 @@ pub fn get_stats_dir(files: &[PathBuf], input_format: &SeqFormat) {
         s.send(get_stats(file, input_format)).unwrap();
     });
 
-    let stats: Vec<(Dna, Sites)> = rec.iter().collect();
+    let stats: Vec<(PathBuf, Dna, Sites)> = rec.iter().collect();
 
-    display_summary(&stats);
+    write_aln_stats(&stats).unwrap();
 }
 
-fn get_stats(path: &Path, input_format: &SeqFormat) -> (Dna, Sites) {
+fn get_stats(path: &Path, input_format: &SeqFormat) -> (PathBuf, Dna, Sites) {
     let mut aln = Alignment::new();
     aln.get_aln_any(path, input_format);
     let mut dna = Dna::new();
-    dna.count_chars(&aln.alignment);
+    dna.count_chars(&aln);
     let mut sites = Sites::new();
     sites.get_stats(&aln.alignment);
 
-    (dna, sites)
+    (PathBuf::from(path), dna, sites)
 }
 
-fn display_summary(stats: &[(Dna, Sites)]) {
-    stats.iter().for_each(|(dna, site)| {
-        display_stats(site, dna).unwrap();
-    })
+fn write_aln_stats(stats: &[(PathBuf, Dna, Sites)]) -> Result<()> {
+    let fname = "SEGUL-stats_per_locus.csv";
+    let file = File::create(fname).expect("CANNOT WRITE THE STAT RESULTS");
+    let mut writer = BufWriter::new(file);
+    writeln!(
+        writer,
+        "
+        dir,\
+        locus_path,\
+        ntaxa,\
+        chars_count,\
+        site_count,\
+        conserved_sites,\
+        proportion_cons_sites,\
+        variable_sites,\
+        proportion_var_sites,\
+        parsimony_informative_sites,\
+        proportion_pars_inf_sites,\
+        AT_content,\
+        GC_content,\
+        A_counts,\
+        T_counts,\
+        G_counts,\
+        C_counts,\
+        gap_counts,\
+        missing_counts,\
+    "
+    )?;
+    stats.iter().for_each(|(path, dna, site)| {
+        write_content(&mut writer, path, dna, site).unwrap();
+    });
+
+    Ok(())
 }
 
-fn display_stats(site: &Sites, dna: &Dna) -> Result<()> {
+fn write_content<W: Write>(writer: &mut W, path: &Path, dna: &Dna, site: &Sites) -> Result<()> {
+    write!(
+        writer,
+        "{},{},{},{},",
+        path.parent().unwrap().display(),
+        path.file_name().unwrap().to_string_lossy(),
+        dna.ntax,
+        dna.nchars
+    )?;
+
+    // Site stats
+    write!(
+        writer,
+        "{},{},{},{},{},{},{},",
+        site.counts,
+        site.conserved,
+        site.prop_cons,
+        site.variable,
+        site.prop_cons,
+        site.pars_inf,
+        site.prop_pinf
+    )?;
+
+    // GC content
+    write!(writer, "{}", (dna.g_count + dna.c_count) / dna.nchars)?;
+
+    // AT content
+    write!(writer, "{}", (dna.a_count + dna.t_count) / dna.nchars)?;
+
+    // Characters
+    writeln!(
+        writer,
+        "{},{},{},{},{},{}",
+        dna.a_count, dna.t_count, dna.c_count, dna.g_count, dna.gaps, dna.missings
+    )?;
+    Ok(())
+}
+
+fn display_stats(site: &Sites, dna: &Dna, aln: &Header) -> Result<()> {
     let io = io::stdout();
     let mut writer = BufWriter::new(io);
+
+    writeln!(writer, "\x1b[0;33mAlignment\x1b[0m")?;
+    writeln!(writer, "Taxa\t\t: {}", utils::fmt_num(&aln.ntax))?;
+    writeln!(writer, "Length\t\t: {}\n", utils::fmt_num(&aln.nchar))?;
 
     writeln!(writer, "\x1b[0;33mSites\x1b[0m")?;
     writeln!(writer, "Count\t\t: {}", utils::fmt_num(&site.counts))?;
     writeln!(writer, "Conserved\t: {}", utils::fmt_num(&site.conserved))?;
     writeln!(writer, "Variable\t: {}", utils::fmt_num(&site.variable))?;
-    writeln!(writer, "Parsimony inf.\t: {}\n", site.pars_inf)?;
+    writeln!(
+        writer,
+        "Parsimony inf.\t: {}\n",
+        utils::fmt_num(&site.pars_inf)
+    )?;
+    writeln!(writer, "Prop. conserved\t: {:.2}%", site.prop_cons * 100.0)?;
+    writeln!(writer, "Prop. variable\t: {:.2}%", site.prop_var * 100.0)?;
+    writeln!(writer, "Prop. p. inf.\t: {:.2}%\n", site.prop_var * 100.0)?;
 
     writeln!(writer, "\x1b[0;33mCharacters\x1b[0m")?;
     writeln!(writer, "Total\t: {}", utils::fmt_num(&dna.nchars))?;
@@ -78,11 +158,15 @@ fn display_stats(site: &Sites, dna: &Dna) -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug)]
 struct Sites {
     conserved: usize,
     variable: usize,
     pars_inf: usize,
     counts: usize,
+    prop_var: f64,
+    prop_cons: f64,
+    prop_pinf: f64,
 }
 
 impl Sites {
@@ -92,6 +176,9 @@ impl Sites {
             variable: 0,
             pars_inf: 0,
             counts: 0,
+            prop_var: 0.0,
+            prop_cons: 0.0,
+            prop_pinf: 0.0,
         }
     }
 
@@ -99,6 +186,7 @@ impl Sites {
         let site_matrix = self.index_sites(matrix);
         self.get_site_stats(&site_matrix);
         self.count_sites();
+        self.get_proportion();
     }
 
     fn index_sites(&mut self, matrix: &IndexMap<String, String>) -> HashMap<usize, Vec<u8>> {
@@ -158,8 +246,15 @@ impl Sites {
     fn count_sites(&mut self) {
         self.counts = self.conserved + self.variable;
     }
+
+    fn get_proportion(&mut self) {
+        self.prop_cons = self.conserved as f64 / self.counts as f64;
+        self.prop_var = self.variable as f64 / self.counts as f64;
+        self.prop_pinf = self.pars_inf as f64 / self.counts as f64;
+    }
 }
 
+#[derive(Debug)]
 struct Dna {
     a_count: usize,
     c_count: usize,
@@ -170,6 +265,7 @@ struct Dna {
     gaps: usize,
     others: usize,
     nchars: usize,
+    ntax: usize,
 }
 
 impl Dna {
@@ -184,23 +280,23 @@ impl Dna {
             gaps: 0,
             others: 0,
             nchars: 0,
+            ntax: 0,
         }
     }
 
-    fn count_chars(&mut self, matrix: &IndexMap<String, String>) {
-        matrix.values().for_each(|seqs| {
-            seqs.bytes().for_each(|ch| {
-                match ch {
-                    b'a' | b'A' => self.a_count += 1,
-                    b'c' | b'C' => self.c_count += 1,
-                    b'g' | b'G' => self.g_count += 1,
-                    b't' | b'T' => self.t_count += 1,
-                    b'n' | b'N' => self.n_count += 1,
-                    b'?' | b'.' => self.missings += 1,
-                    b'-' => self.gaps += 1,
-                    _ => self.others += 1,
-                }
-                self.nchars += 1;
+    fn count_chars(&mut self, aln: &Alignment) {
+        self.ntax = aln.header.ntax;
+        self.nchars = aln.header.nchar * self.ntax;
+        aln.alignment.values().for_each(|seqs| {
+            seqs.bytes().for_each(|ch| match ch {
+                b'a' | b'A' => self.a_count += 1,
+                b'c' | b'C' => self.c_count += 1,
+                b'g' | b'G' => self.g_count += 1,
+                b't' | b'T' => self.t_count += 1,
+                b'n' | b'N' => self.n_count += 1,
+                b'?' | b'.' => self.missings += 1,
+                b'-' => self.gaps += 1,
+                _ => self.others += 1,
             })
         })
     }
