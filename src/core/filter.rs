a66d520f333d2dcb718bc850a81dcabb1025ea6e
Heru Handika <herubiolog@gmail.com>
2021-07-10 17:14:32 -0500

    Remove atomic size. Collect the matched files to a vector first instead of copying them right away.

--- src/core/filter.rs
+++ src/core/filter.rs
@@ -1,7 +1,8 @@
-use core::sync::atomic::{AtomicUsize, Ordering};
+// use core::sync::atomic::{AtomicUsize, Ordering};
 use std::fs;
 use std::io::{self, BufWriter, Result, Write};
 use std::path::{Path, PathBuf};
+use std::sync::mpsc::channel;
 
 use rayon::prelude::*;
 
@@ -44,17 +45,37 @@ impl<'a> SeqFilter<'a> {
         self.min_taxa = self.count_min_tax();
         self.display_input().expect("CANNOT DISPLAY TO STDOUT");
         fs::create_dir_all(self.output_dir).expect("CANNOT CREATE A TARGET DIRECTORY");
-        let fcounts = AtomicUsize::new(0);
-        self.files.par_iter().for_each(|file| {
+        let match_aln = self.par_match_aln();
+        self.par_copy_files(&match_aln);
+        self.display_output(match_aln.len())
+            .expect("CANNOT DISPLAY TO STDOUT");
+    }
+
+    fn par_match_aln(&self) -> Vec<PathBuf> {
+        let (send, rx) = channel();
+        self.files.par_iter().for_each_with(send, |s, file| {
             let header = self.get_header(file);
             if header.ntax >= self.min_taxa {
-                self.copy_files(file).expect("CANNOT COPY FILES");
-                fcounts.fetch_add(1, Ordering::Relaxed);
+                s.send(file.to_path_buf()).expect("FAILED GETTING FILES");
             }
         });
 
-        self.display_output(fcounts.load(Ordering::Relaxed))
-            .expect("CANNOT DISPLAY TO STDOUT");
+        rx.iter().collect()
+    }
+
+    fn par_copy_files(&self, match_path: &[PathBuf]) {
+        match_path.par_iter().for_each(|path| {
+            self.copy_files(path).expect("CANNOT COPY FILES");
+        });
+    }
+
+    fn copy_files(&self, origin: &Path) -> Result<()> {
+        let fname = origin.file_name().unwrap();
+        let destination = self.output_dir.join(fname);
+
+        fs::copy(origin, destination)?;
+
+        Ok(())
     }
 
     fn display_input(&self) -> Result<()> {
@@ -77,15 +98,6 @@ impl<'a> SeqFilter<'a> {
         writeln!(writer, "\n\x1b[0;33mOutput\x1b[0m")?;
         writeln!(writer, "File count\t: {}", utils::fmt_num(&fcounts))?;
         writeln!(writer, "Dir\t\t: {}", self.output_dir.display())?;
-
-        Ok(())
-    }
-
-    fn copy_files(&self, origin: &Path) -> Result<()> {
-        let fname = origin.file_name().unwrap();
-        let destination = self.output_dir.join(fname);
-
-        fs::copy(origin, destination)?;
 
         Ok(())
     }
