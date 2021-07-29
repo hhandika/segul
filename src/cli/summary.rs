8bcd00dbdf35be7124c42e3463b79211f082c42e
Heru Handika <herubiolog@gmail.com>
2021-07-29 15:59:54 -0500

    Clean terminal and log output.

--- src/cli/summary.rs
+++ src/cli/summary.rs
@@ -41,17 +41,18 @@ impl<'a> SummaryParser<'a> {
         self.input_fmt = self.parse_input_fmt(&self.matches);
         self.interval = self.parse_interval();
         self.datatype = self.parse_datatype(self.matches);
-        self.print_input_file().unwrap();
         let input_type = self.parse_input_type(&self.matches);
         match input_type {
             InputType::File => self.get_stats_file(),
             InputType::Dir => {
                 let dir = self.parse_dir_input(self.matches);
                 let files = self.get_files(dir, &self.input_fmt);
+                self.print_input_multi(Some(dir), files.len());
                 self.get_stats_multiple(&files);
             }
             InputType::Wildcard => {
                 let files = self.parse_input_wcard(&self.matches);
+                self.print_input_multi::<PathBuf>(None, files.len());
                 self.get_stats_multiple(&files)
             }
         }
@@ -66,7 +67,7 @@ impl<'a> SummaryParser<'a> {
         self.parse_input_fmt(&self.matches);
         let input = Path::new(self.parse_file_input(self.matches));
         let output = self.parse_output(&self.matches);
-        self.print_input_file().unwrap();
+        self.print_input_file(&input);
         SeqStats::new(&self.input_fmt, output, self.interval, &self.datatype)
             .get_seq_stats_file(input);
     }
@@ -81,25 +82,29 @@ impl<'a> SummaryParser<'a> {
             .expect("FAIL PARSING INTERVAL VALUES")
     }
 
-    fn is_input_dir(&self) -> bool {
-        self.matches.is_present("dir")
+    fn print_input_file(&self, input: &Path) {
+        log::info!("{:18}: {}", "Input", &input.display());
+        self.print_input_fmt();
+        log::info!("{:18}: Sequence summary statistics\n", "Task");
     }
 
-    // FIXME: This does not cover all cases
-    fn print_input_file(&self) -> Result<()> {
-        let io = io::stdout();
-        let mut writer = BufWriter::new(io);
-        writeln!(writer, "Command\t\t: segul summary")?;
-        if self.is_input_dir() {
-            writeln!(
-                writer,
-                "Input dir\t: {}\n",
-                self.parse_dir_input(self.matches)
-            )?;
+    fn print_input_multi<P: AsRef<Path>>(&self, input: Option<P>, fcounts: usize) {
+        if let Some(input) = input {
+            log::info!("{:18}: {}", "Input dir", &input.as_ref().display());
         } else {
-            writeln!(writer, "Input\t\t: WILDCARD\n",)?;
+            log::info!("{:18}: {}", "Input dir", "WILDCARD");
         }
+        log::info!("{:18}: {}", "File counts", utils::fmt_num(&fcounts));
+        self.print_input_fmt();
+        log::info!("{:18}: Sequence summary statistics\n", "Task");
+    }
 
-        Ok(())
+    fn print_input_fmt(&self) {
+        match self.input_fmt {
+            InputFmt::Auto => log::info!("{:18}: {}", "Input format", "Auto"),
+            InputFmt::Fasta => log::info!("{:18}: {}", "Input format", "Fasta"),
+            InputFmt::Nexus => log::info!("{:18}: {}", "Input format", "Nexus"),
+            InputFmt::Phylip => log::info!("{:18}: {}", "Input format", "Phylip"),
+        }
     }
 }
