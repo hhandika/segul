224b77a9b34cf5b595520e8fa8a60802e569a4e9
hhandika <herubiolog@gmail.com>
2021-08-09 12:35:21 -0500

    Remove unused crates.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -19,7 +19,6 @@ use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
-use rayon::prelude::*;
 
 use crate::cli::concat::ConcatParser;
 use crate::cli::convert::ConvertParser;
