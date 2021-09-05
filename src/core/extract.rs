0ef8751ba3085989c7757e171b37af1710799960
Heru Handika <herubiolog@gmail.com>
2021-09-05 13:48:24 -0500

    Add matrix test.

--- src/core/extract.rs
+++ src/core/extract.rs
@@ -126,12 +126,22 @@ mod tests {
     }
 
     #[test]
-    fn test_get_matrix() {
+    fn test_get_matrix_re() {
         let re = Params::Regex(String::from("(?i)(celebensis)"));
         let file = Path::new("test_files/complete.nex");
         let extract = Extract::new(&re, &InputFmt::Nexus, &DataType::Dna);
         let (seq, _) = Sequence::new(file, extract.datatype).get(extract.input_fmt);
         let matrix = extract.get_matrix(seq);
         assert_eq!(2, matrix.len());
+    }
+
+    #[test]
+    fn test_get_matrix_id() {
+        let re = Params::Id(vec![String::from("Taeromys_calitrichus_NMVZ27408")]);
+        let file = Path::new("test_files/complete.nex");
+        let extract = Extract::new(&re, &InputFmt::Nexus, &DataType::Dna);
+        let (seq, _) = Sequence::new(file, extract.datatype).get(extract.input_fmt);
+        let matrix = extract.get_matrix(seq);
+        assert_eq!(1, matrix.len());
     }
 }
