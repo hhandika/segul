d1dce8bdeaa4230d1ff8ce8a37e061c55991334e
hhandika <herubiolog@gmail.com>
2021-12-21 13:03:16 -0600

    Fix issue in creating output path.

--- src/core/convert.rs
+++ src/core/convert.rs
@@ -34,8 +34,7 @@ impl<'a> Converter<'a> {
         let spin = utils::set_spinner();
         spin.set_message("Converting sequence format...");
         files.par_iter().for_each(|file| {
-            let fname = output.join(file.file_stem().unwrap());
-            let output_fname = filenames::create_output_fname(&fname, file, self.output_fmt);
+            let output_fname = filenames::create_output_fname(&output, file, self.output_fmt);
             self.convert_any(file, &output_fname);
         });
         spin.finish_with_message("Finished converting sequence format!\n");
