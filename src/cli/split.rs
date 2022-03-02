58a7eb25276bd4dab755a665cd8e97731926093a
hhandika <herubiolog@gmail.com>
2022-03-02 17:30:27 -0600

    Add split functions

--- src/cli/split.rs
+++ src/cli/split.rs
@@ -1,1 +1,64 @@
+use std::path::{Path, PathBuf};
 
+use clap::ArgMatches;
+
+use crate::cli::{InputCli, InputPrint, OutputCli};
+use crate::core::split::Splitter;
+use crate::helper::types::PartitionFmt;
+
+impl InputPrint for SplitParser<'_> {}
+impl OutputCli for SplitParser<'_> {}
+impl InputCli for SplitParser<'_> {}
+
+pub(in crate::cli) struct SplitParser<'a> {
+    matches: &'a ArgMatches<'a>,
+}
+
+impl<'a> SplitParser<'a> {
+    pub(in crate::cli) fn new(matches: &'a ArgMatches<'a>) -> Self {
+        Self { matches }
+    }
+
+    pub(in crate::cli) fn split(&mut self) {
+        let input = self.parse_input_path();
+        let input_fmt = self.parse_input_fmt(self.matches);
+        let datatype = self.parse_datatype(self.matches);
+        let output_fmt = self.parse_output_fmt(self.matches);
+        let output = self.parse_output(self.matches);
+        let partitions = self.parse_partition_path();
+        let part_fmt = self.parse_partition_fmt(&partitions);
+        let task_desc = "Alignment splitting";
+        self.print_input(&None::<PathBuf>, task_desc, 1, &input_fmt, &datatype);
+        let split = Splitter::new(&input, &datatype, &input_fmt, &output, &output_fmt);
+        split.split_alignment(&partitions, &part_fmt);
+    }
+
+    fn parse_input_path(&self) -> PathBuf {
+        let input_file = self
+            .matches
+            .value_of("input")
+            .expect("Input file is required");
+        PathBuf::from(input_file)
+    }
+
+    fn parse_partition_path(&self) -> PathBuf {
+        PathBuf::from(
+            self.matches
+                .value_of("partition")
+                .expect("No partition file provided"),
+        )
+    }
+
+    fn parse_partition_fmt(&self, part_path: &Path) -> PartitionFmt {
+        let ext = part_path
+            .extension()
+            .expect("Failed getting file extension")
+            .to_str()
+            .expect("Failed getting file extension as string");
+        match ext {
+            "txt" | "raxml" => PartitionFmt::Raxml,
+            "nex" | "nexus" => PartitionFmt::Nexus,
+            _ => panic!("Unsupported partition file format"),
+        }
+    }
+}
--- src/core/split.rs
+++ src/core/split.rs
@@ -1,16 +1,20 @@
-#[allow(dead_code, unused_imports)]
-use std::ffi::OsStr;
 use std::path::Path;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-#[allow(dead_code, unused_imports)]
-use crate::helper::finder::IDs;
+use ansi_term::Colour::Yellow;
+use indexmap::IndexMap;
+use rayon::prelude::*;
+
+use crate::core::OutputPrint;
+use crate::helper::filenames;
 use crate::helper::sequence::Sequence;
-use crate::helper::types::{DataType, InputFmt, OutputFmt, PartitionFmt, SeqMatrix};
-// use crate::helper::utils;
+use crate::helper::types::{DataType, Header, InputFmt, OutputFmt, PartitionFmt, SeqMatrix};
+use crate::helper::utils;
 use crate::parser::partition::PartitionParser;
-// use crate::writer::sequences::SeqWriter;
+use crate::writer::sequences::SeqWriter;
 
-#[allow(dead_code)]
+impl OutputPrint for Splitter<'_> {}
+
 pub struct Splitter<'a> {
     input: &'a Path,
     datatype: &'a DataType,
@@ -19,7 +23,6 @@ pub struct Splitter<'a> {
     output_fmt: &'a OutputFmt,
 }
 
-#[allow(dead_code, unused_variables)]
 impl<'a> Splitter<'a> {
     pub fn new(
         input: &'a Path,
@@ -37,13 +40,62 @@ impl<'a> Splitter<'a> {
         }
     }
 
-    fn split_alignment(&self, partition: &Path, partition_fmt: &PartitionFmt) {
+    pub fn split_alignment(&self, partition: &Path, partition_fmt: &PartitionFmt) {
         let partitions = PartitionParser::new(partition, partition_fmt).parse();
+        let spin = utils::set_spinner();
+        spin.set_message("Parsing input sequence file...");
         let aln_matrix = self.parse_sequence();
+        spin.set_message("Splitting alignment...");
+        let file_counts = AtomicUsize::new(0);
+        partitions.par_iter().for_each(|part| {
+            let start_pos = part.start - 1;
+            let end_pos = part.end;
+            let matrix = self.generate_new_matrix(&aln_matrix, start_pos, end_pos);
+            let mut header = Header::new();
+            if let DataType::Aa = self.datatype {
+                header.datatype = String::from("protein");
+            }
+            header.nchar = start_pos - end_pos;
+            header.ntax = matrix.len();
+            header.aligned = true;
+            let filename = Path::new(&part.gene);
+            let output_path =
+                filenames::create_output_fname(self.output, filename, self.output_fmt);
+            let mut out = SeqWriter::new(&output_path, &matrix, &header, None, &PartitionFmt::None);
+            out.write_sequence(self.output_fmt)
+                .expect("Failed writing the output file");
+            file_counts.fetch_add(1, Ordering::SeqCst);
+        });
+
+        spin.finish_with_message("Finished splitting alignment!\n");
+
+        self.print_output_info(file_counts.load(Ordering::SeqCst));
     }
 
-    fn slice_sequence(&self, seq: &str, start: &usize, end: &usize) -> String {
-        seq.get(*start..*end)
+    fn print_output_info(&self, file_counts: usize) {
+        log::info!("{}", Yellow.paint("Output"));
+        log::info!("{:18}: {}", "File counts", file_counts);
+        log::info!("{:18}: {}", "Output dir", self.output.display());
+        self.print_output_fmt(self.output_fmt);
+    }
+
+    fn generate_new_matrix(
+        &self,
+        matrix: &SeqMatrix,
+        start_pos: usize,
+        end_pos: usize,
+    ) -> SeqMatrix {
+        let mut seq_matrix: SeqMatrix = IndexMap::new();
+        matrix.iter().for_each(|(taxon, seq)| {
+            // We substract the start position to match rust indexing.
+            let part_seq = self.slice_sequence(seq, start_pos, end_pos);
+            seq_matrix.insert(taxon.clone(), part_seq);
+        });
+        seq_matrix
+    }
+
+    fn slice_sequence(&self, seq: &str, start: usize, end: usize) -> String {
+        seq.get(start..end)
             .expect("Errors in slicing the alignment")
             .to_string()
     }
@@ -70,8 +122,8 @@ mod test {
         );
         let seq = "AAAAAAAAGGGGTTTTCCCC";
 
-        let slice = split.slice_sequence(seq, &0, &10);
-        let slice_2 = split.slice_sequence(seq, &10, &15);
+        let slice = split.slice_sequence(seq, 0, 10);
+        let slice_2 = split.slice_sequence(seq, 10, 15);
         assert_eq!(slice, "AAAAAAAAGG");
         assert_eq!(slice_2, "GGTTT");
     }
