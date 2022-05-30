21ce3a62890c2a67895d84d46d4ee5b61bd1d2bb
hhandika <herubiolog@gmail.com>
2022-05-30 22:07:04 +0700

    Fix id option conflicts for extract function.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,12 +2,22 @@ use assert_cmd::Command;
 
 fn segul() -> Command {
     let mut cmd = Command::cargo_bin("segul").unwrap();
-    let dir = std::env::current_dir().unwrap().join("tests").join("files");
-    cmd.current_dir(dir);
+    cmd.current_dir("test/files");
     cmd
 }
 
 #[test]
 fn test_bin() {
     segul().arg("-V").assert().success();
+}
+
+#[test]
+fn test_convert() {
+    segul()
+        .arg("-i")
+        .arg("test.txt")
+        .arg("-o")
+        .arg("test.csv")
+        .assert()
+        .success();
 }
