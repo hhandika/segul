4ce65f067131ed8bf9f80a4f81391038a4729842
Heru Handika <herubiolog@gmail.com>
2022-06-29 15:29:59 -0500

    Add subcommand test.

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,6 +6,15 @@ fn segul() -> Command {
     cmd
 }
 
+macro_rules! test_subcommand {
+    ($func: ident, $subcommand: expr) => {
+        #[test]
+        fn $func() {
+            segul().arg($subcommand).arg("-h").assert().success();
+        }
+    };
+}
+
 #[test]
 fn test_version() {
     use clap::crate_version;
@@ -16,3 +25,20 @@ fn test_version() {
         .success()
         .stdout(predicates::str::contains(version));
 }
+
+test_subcommand! {test_concat_subcommand, "concat"}
+test_subcommand! {test_convert_subcommand, "convert"}
+test_subcommand! {test_extract_subcommand, "extract"}
+test_subcommand! {test_filter_subcommand, "filter"}
+test_subcommand! {test_id_subcommand, "id"}
+// test_subcommand! {test_partition_subcommand, "partition"}
+// test_subcommand! {test_remove_subcommand, "remove"}
+// test_subcommand! {test_rename_subcommand, "rename"}
+// test_subcommand! {test_split_subcommand, "split"}
+test_subcommand! {test_summary_subcommand, "summary"}
+test_subcommand! {test_translate_subcommand, "translate"}
+
+// #[test]
+// fn test_part_subcommand() {
+//     segul().arg("remove").assert().success();
+// }
