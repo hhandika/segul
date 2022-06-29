9b36e5af1802b7c5ba9ee135e516d9979361c983
Heru Handika <herubiolog@gmail.com>
2022-06-29 15:29:11 -0500

    Add remove id implementation.

--- src/cli/remove.rs
+++ src/cli/remove.rs
@@ -1,1 +1,83 @@
+use std::path::PathBuf;
 
+use ansi_term::Colour::Yellow;
+use clap::ArgMatches;
+
+// use crate::helper::utils;
+use crate::{
+    cli::{InputCli, InputPrint, OutputCli},
+    handler::remove::{Remove, RemoveOpts},
+};
+
+impl InputCli for RemoveParser<'_> {}
+impl InputPrint for RemoveParser<'_> {}
+impl OutputCli for RemoveParser<'_> {}
+
+pub(in crate::cli) struct RemoveParser<'a> {
+    matches: &'a ArgMatches,
+    input_dir: Option<PathBuf>,
+}
+
+impl<'a> RemoveParser<'a> {
+    pub(in crate::cli) fn new(matches: &'a ArgMatches) -> Self {
+        Self {
+            matches,
+            input_dir: None,
+        }
+    }
+
+    pub(in crate::cli) fn remove(&mut self) {
+        let input_fmt = self.parse_input_fmt(self.matches);
+        let datatype = self.parse_datatype(self.matches);
+        let output_fmt = self.parse_output_fmt(self.matches);
+        let outdir = self.parse_output(self.matches);
+        let task_desc = "Sequence Renaming";
+        let files = if self.matches.is_present("dir") {
+            let dir = self.parse_dir_input(self.matches);
+            self.input_dir = Some(PathBuf::from(dir));
+            self.get_files(dir, &input_fmt)
+        } else {
+            self.parse_input(self.matches)
+        };
+
+        self.print_input(
+            &self.input_dir,
+            task_desc,
+            files.len(),
+            &input_fmt,
+            &datatype,
+        );
+        let opts = self.parse_remove_opts();
+
+        let is_overwrite = self.parse_overwrite_opts(self.matches);
+        self.check_output_dir_exist(&outdir, is_overwrite);
+        Remove::new(&input_fmt, &datatype, &outdir, &output_fmt, &opts).remove(&files);
+    }
+
+    fn parse_remove_opts(&self) -> RemoveOpts {
+        log::info!("{}", Yellow.paint("Params"));
+        match self.matches {
+            m if m.is_present("id") => {
+                let ids = self
+                    .matches
+                    .values_of("id")
+                    .expect("Failed parsing ids")
+                    .map(String::from)
+                    .collect();
+                log::info!("{:18}: id", "Options");
+                log::info!("{:18}, {:?}", "Values", ids);
+                RemoveOpts::Id(ids)
+            }
+
+            m if m.is_present("re") => {
+                let input_re = self
+                    .matches
+                    .value_of("re")
+                    .expect("Failed parsing regex values")
+                    .to_string();
+                RemoveOpts::Regex(input_re)
+            }
+            _ => unimplemented!("Unknown errors in parsing command line input!"),
+        }
+    }
+}
--- src/handler/remove.rs
+++ src/handler/remove.rs
@@ -1,7 +1,15 @@
 use std::path::{Path, PathBuf};
 
+use ansi_term::Colour::Yellow;
+
+use crate::handler::OutputPrint;
+use crate::helper::filenames;
+use crate::helper::sequence::{SeqCheck, SeqParser};
 use crate::helper::types::{DataType, Header, InputFmt, OutputFmt, SeqMatrix};
 use crate::helper::utils;
+use crate::writer::sequences::SeqWriter;
+
+impl OutputPrint for Remove<'_> {}
 
 pub enum RemoveOpts {
     Id(Vec<String>),
@@ -33,13 +41,52 @@ impl<'a> Remove<'a> {
         }
     }
 
-    pub fn remove(&self, file: &[PathBuf]) {
+    pub fn remove(&self, files: &[PathBuf]) {
         let spin = utils::set_spinner();
         spin.set_message("Removing sequences...");
         match self.opts {
-            RemoveOpts::Id(names) => {}
-            RemoveOpts::Regex(input_re) => {}
+            RemoveOpts::Id(ids) => files.iter().for_each(|file| {
+                let (matrix, header) = self.remove_sequence(file, ids);
+                self.write_output(&matrix, &header, file);
+            }),
+            RemoveOpts::Regex(input_re) => println!("Regex {} is working!", input_re),
         }
         spin.finish_with_message("Finished removing sequences!\n");
+        self.print_output_info();
+    }
+
+    fn write_output(&self, matrix: &SeqMatrix, header: &Header, file: &Path) {
+        let outpath = filenames::create_output_fname(self.outdir, file, self.output_fmt);
+        let mut writer = SeqWriter::new(&outpath, &matrix, &header);
+        writer
+            .write_sequence(self.output_fmt)
+            .expect("Failed writing output sequence");
+    }
+
+    fn remove_sequence(&self, fpath: &Path, ids: &[String]) -> (SeqMatrix, Header) {
+        let (mut matrix, _) = SeqParser::new(fpath, self.datatype).get(self.input_fmt);
+        ids.iter().for_each(|id| match matrix.remove(id) {
+            Some(_) => (),
+            None => (),
+        });
+
+        let header = self.get_header(&matrix);
+        (matrix, header)
+    }
+
+    fn get_header(&self, matrix: &SeqMatrix) -> Header {
+        let mut seq_info = SeqCheck::new();
+        seq_info.check(matrix);
+        let mut header = Header::new();
+        header.aligned = seq_info.is_alignment;
+        header.nchar = seq_info.longest;
+        header.ntax = matrix.len();
+        header
+    }
+
+    fn print_output_info(&self) {
+        log::info!("{}", Yellow.paint("Output"));
+        log::info!("{:18}: {}", "Output dir", self.outdir.display());
+        self.print_output_fmt(self.output_fmt);
     }
 }
