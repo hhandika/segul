63394f73cafe35ab9d21553e7b9a176960212201
Heru Handika <herubiolog@gmail.com>
2022-07-01 12:03:57 -0500

    Test for regex.

--- tests/extract.rs
+++ tests/extract.rs
@@ -22,3 +22,19 @@ fn test_extract() {
 
     tmp_dir.close().unwrap();
 }
+
+#[test]
+fn test_extract_re() {
+    initiate_cmd!(cmd, "extract", "tests/files/concat/", tmp_dir);
+    cmd.arg("--re=^AB").assert().success();
+
+    let pred = predicates::path::is_dir();
+    let res_path = tmp_dir.path().join("SEGUL-Extract");
+
+    let files = Files::new(&res_path, &InputFmt::Nexus).find();
+
+    assert!(pred.eval(&res_path));
+    assert_eq!(4, files.len());
+
+    tmp_dir.close().unwrap();
+}
--- tests/remove.rs
+++ tests/remove.rs
@@ -22,3 +22,19 @@ fn test_remove_id() {
 
     tmp_dir.close().unwrap();
 }
+
+#[test]
+fn test_remove_re() {
+    initiate_cmd!(cmd, "remove", "tests/files/concat/", tmp_dir);
+    cmd.arg("--re=E$").assert().success();
+
+    let pred = predicates::path::is_dir();
+    let res_path = tmp_dir.path().join("SEGUL-Remove");
+
+    let files = Files::new(&res_path, &InputFmt::Nexus).find();
+
+    assert!(pred.eval(&res_path));
+    assert_eq!(4, files.len());
+
+    tmp_dir.close().unwrap();
+}
