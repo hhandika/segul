5c5c3fe49081219cc41a83da9df2ae74d5529b95
Heru Handika <herubiolog@gmail.com>
2022-07-01 11:39:28 -0500

    Add filter integration test.

--- tests/filter.rs
+++ tests/filter.rs
@@ -7,22 +7,45 @@ use predicates::Predicate;
 use segul::helper::finder::Files;
 use segul::helper::types::InputFmt;
 
+macro_rules! test_filter {
+    ($test: ident, $arg: expr, $val: expr, $res: expr) => {
+        #[test]
+        fn $test() {
+            initiate_cmd!(cmd, "filter", "tests/files/filter", tmp_dir);
+            let output = tmp_dir.path().join("concat_50p");
+            cmd.arg($arg)
+                .arg($val)
+                .arg("-o")
+                .arg(&output)
+                .assert()
+                .success();
+
+            let pred = predicates::path::is_dir();
+            let files = Files::new(&output, &InputFmt::Nexus).find();
+
+            assert!(pred.eval(&output));
+            assert_eq!($res, files.len());
+
+            tmp_dir.close().unwrap();
+        }
+    };
+}
+
+test_filter! {test_filter_percent, "--percent", "0.5", 4}
+test_filter! {test_percent_pinf, "--pinf", "3", 1}
+test_filter! {test_percent_percent_inf, "--percent-inf", ".5", 4}
+test_filter! {test_percent_len, "--len", "25", 4}
+
 #[test]
-fn test_extract() {
-    initiate_cmd!(cmd, "filter", tmp_dir);
+#[should_panic]
+fn test_filter_panic() {
+    initiate_cmd!(cmd, "filter", "tests/files/filter", tmp_dir);
     let output = tmp_dir.path().join("concat_50p");
-    cmd.arg("--percent")
-        .arg("0.5")
+    cmd.arg("--len")
+        .arg("27")
         .arg("-o")
         .arg(&output)
         .assert()
         .success();
-
-    let pred = predicates::path::is_dir();
-    let files = Files::new(&output, &InputFmt::Nexus).find();
-
-    assert!(pred.eval(&output));
-    assert_eq!(4, files.len());
-
     tmp_dir.close().unwrap();
 }
