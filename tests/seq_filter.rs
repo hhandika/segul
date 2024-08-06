958dc14616668bb4bc6ddc355311521ddb5b5208
Heru Handika <herubiolog@gmail.com>
2024-08-05 20:05:45 -0500

    Update tests.

--- tests/seq_filter.rs
+++ tests/seq_filter.rs
@@ -8,8 +8,15 @@ use segul::helper::finder::SeqFileFinder;
 use segul::helper::types::InputFmt;
 
 #[test]
-fn test_sequence_filter() {
+fn test_sequence_filter_gaps() {
     initiate_cmd!(cmd, "sequence", "filter", "tests/files/gappy/", tmp_dir);
     cmd.arg("--max-gap").arg(".5").assert().success();
     test_results!(4, tmp_dir, "Sequence-Filter", Nexus);
+}
+
+#[test]
+fn test_sequence_filter_length() {
+    initiate_cmd!(cmd, "sequence", "filter", "tests/files/gappy/", tmp_dir);
+    cmd.arg("--min-length").arg("10").assert().success();
+    test_results!(1, tmp_dir, "Sequence-Filter", Nexus);
 }
